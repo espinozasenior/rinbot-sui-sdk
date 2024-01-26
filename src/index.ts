8f5dd2d9fb34d03a1d0872521fcc70539e887109
avernikoz <avernikoz@gmail.com>
2024-01-26 21:09:05 +0300

    add exports from index.ts

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+// Managers
+export * from "./managers/CoinManager";
+export * from "./managers/RouteManager";
+export * from "./managers/WalletManager";
+export * from "./managers/types";
+
+// Providers (common & utils)
+export * from "./providers/common";
+export * from "./providers/utils/convertSlippage";
+export * from "./providers/utils/convertToInteger";
+export * from "./providers/utils/isValidTokenAddress";
+export * from "./providers/utils/normalizeSuiAddress";
+export * from "./providers/utils/tryCatchWrapper";
+export * from "./providers/utils/getSuiProvider";
+export * from "./providers/utils/isValidTokenAmount";
+
+// Aftermath
+export * from "./providers/aftermath/aftermath";
+export * from "./providers/aftermath/types";
+
+// Cetus
+export * from "./providers/cetus/cetus";
+export * from "./providers/cetus/config";
+
+// FlowX
+export * from "./providers/flowx/flowx";
+
+// Turbos
+export * from "./providers/turbos/turbos";
+export * from "./providers/turbos/types";
+export * from "./providers/turbos/utils";
+
+export { SUI_DECIMALS, isValidSuiAddress } from "@mysten/sui.js/utils";
