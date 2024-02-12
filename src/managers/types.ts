a69d791c4538dac319dcf57aa2b3c86ced7173d5
avernikoz <avernikoz@gmail.com>
2024-02-12 21:02:32 +0300

    update manager types

--- src/managers/types.ts
+++ src/managers/types.ts
@@ -1,3 +1,4 @@
+import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { AftermathSingleton } from "../providers/aftermath/aftermath";
 import { CetusSingleton } from "../providers/cetus/cetus";
 import { FlowxSingleton } from "../providers/flowx/flowx";
@@ -10,7 +11,11 @@ export type CommonCoinData = {
   decimals: number;
 };
 
-export type CoinAssetData = CommonCoinData & { balance: string };
+export type CoinAssetData = Omit<CommonCoinData, "decimals"> & {
+  balance: string;
+  noDecimals: boolean;
+  decimals: number | null;
+};
 
 export type UpdatedCoinsCache = { provider: string; data: CommonCoinData[] };
 
@@ -27,3 +32,33 @@ export type ProvidersToRouteDataMap = Map<
 >;
 
 export type UpdateCoinsCacheHandler = (arg: UpdatedCoinsCache) => void;
+
+export interface ICoinManager {
+  getCoinByType(coinType: string): CommonCoinData;
+  getCoinByType2(coinType: string): CommonCoinData | null;
+  getCoinsByProviderMap(): Map<string, Map<string, CommonCoinData>>;
+  getAllCoins(): Map<string, CommonCoinData>;
+}
+
+export interface IWalletManager {
+  getAvailableWithdrawSuiAmount(publicKey: string): Promise<{ availableAmount: string; totalGasFee: string }>;
+  getSuiBalance(publicKey: string): Promise<string>;
+  getAvailableSuiBalance(publicKey: string): Promise<string>;
+  getAllCoinAssets(publicKey: string): Promise<CoinAssetData[]>;
+}
+
+export interface IRouteManager {
+  getBestRouteTransaction({
+    tokenFrom,
+    tokenTo,
+    amount,
+    slippagePercentage,
+    signerAddress,
+  }: {
+    tokenFrom: string;
+    tokenTo: string;
+    amount: string;
+    slippagePercentage: number;
+    signerAddress: string;
+  }): Promise<TransactionBlock>;
+}
