61da0a9e77e21e3727b695f8810929676d88e006
avernikoz <avernikoz@gmail.com>
2024-03-05 18:59:22 +0300

    fix eslint stuff

--- src/launchpad/surfdog/__generated__/meme/init.ts
+++ src/launchpad/surfdog/__generated__/meme/init.ts
@@ -1,10 +1,10 @@
 import * as meme from "./meme/structs";
 import * as surf from "./surf/structs";
-import {StructClassLoader} from "../_framework/loader";
+import { StructClassLoader } from "../_framework/loader";
 
 export function registerClasses(loader: StructClassLoader) {
-    loader.register(meme.MemCoinOwnerCap);
-    loader.register(meme.State);
-    loader.register(meme.UserState);
-    loader.register(surf.SURF);
+  loader.register(meme.MemCoinOwnerCap);
+  loader.register(meme.State);
+  loader.register(meme.UserState);
+  loader.register(surf.SURF);
 }
