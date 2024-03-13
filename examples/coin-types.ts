7ac2243c6f357fe00a354f46f8c50517d8ef2fb9
avernikoz <avernikoz@gmail.com>
2024-03-13 22:00:07 +0300

    add rincel cointype

--- examples/coin-types.ts
+++ examples/coin-types.ts
@@ -2,3 +2,4 @@ export const CETUS_COIN_TYPE = "0x06864a6f921804860930db6ddbe2e16acdf8504495ea74
 export const USDC_COIN_TYPE = "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN";
 export const BUCK_COIN_TYPE = "0xce7ff77a83ea0cb6fd39bd8748e2ec89a3f41e8efdc3f4eb123e0ca37b184db2::buck::BUCK";
 export const FUD_COIN_TYPE = "0x76cb819b01abed502bee8a702b4c2d547532c12f25001c9dea795a5e631c26f1::fud::FUD";
+export const RINCEL_COIN_TYPE = "0xd2c7943bdb372a25c2ac7fa6ab86eb9abeeaa17d8d65e7dcff4c24880eac860b::rincel::RINCEL";
