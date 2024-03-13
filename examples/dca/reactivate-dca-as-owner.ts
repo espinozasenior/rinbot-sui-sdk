e4a35d002f4d1aaae2ce4de32f6c99914acb54cd
avernikoz <avernikoz@gmail.com>
2024-03-14 01:22:27 +0300

    upd reactivateDCAAsOwner example

--- examples/dca/reactivate-dca-as-owner.ts
+++ examples/dca/reactivate-dca-as-owner.ts
@@ -5,13 +5,13 @@ import { user } from "../common";
 
 // yarn ts-node examples/dca/reactivate-dca-as-owner.ts
 export const reactivateDCAAsOwner = async () => {
-  const suiProviderUrl = "https://fullnode.testnet.sui.io";
+  const suiProviderUrl = "https://fullnode.mainnet.sui.io";
   const provider = new SuiClient({ url: suiProviderUrl });
   const transaction = new TransactionBlock();
 
   const dcaInstance = DCAManagerSingleton.getInstance(suiProviderUrl);
   const dcas = await dcaInstance.getDCAsByUser({ publicKey: user });
-  const desiredObjectId = "0x3d8999900847d0c7ccca6f965bc02041c478b582aa03e1708d603f7a92358402";
+  const desiredObjectId = "0x4f9fcd90fb8e852899d45693e196d49b07a579ffbc1ca40292cd07f9e9675bdd";
 
   const currentDCAData = dcas.find((el) => el.fields.id.id === desiredObjectId);
   console.debug("currentDCAData: ", currentDCAData);
