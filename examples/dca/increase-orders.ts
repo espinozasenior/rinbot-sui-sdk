e597d9034c05b010ae063a7ac1029a65f29a46db
avernikoz <avernikoz@gmail.com>
2024-03-17 20:04:10 +0300

    upd increase-orders

--- examples/dca/increase-orders.ts
+++ examples/dca/increase-orders.ts
@@ -1,7 +1,8 @@
 import { SuiClient } from "@mysten/sui.js/client";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { DCAManagerSingleton } from "../../src/managers/dca/DCAManager";
-import { keypair, user } from "../common";
+import { keypair, signAndExecuteTransaction, user } from "../common";
+import { delegateeKeypair } from "./common";
 
 // yarn ts-node examples/dca/increase-orders.ts
 export const increaseOrders = async () => {
@@ -12,7 +13,7 @@ export const increaseOrders = async () => {
 
   const dcaInstance = DCAManagerSingleton.getInstance(suiProviderUrl);
   const dcas = await dcaInstance.getDCAsByUser({ publicKey: sender });
-  const desiredObjectId = "0x4f9fcd90fb8e852899d45693e196d49b07a579ffbc1ca40292cd07f9e9675bdd";
+  const desiredObjectId = "0x4d0316c3a32221e175ab2bb9abe360ed1d4498806dc50984ab67ce0ba90f2842";
 
   const currentDCAData = dcas.find((el) => el.fields.id.id === desiredObjectId);
   console.debug("currentDCAData: ", currentDCAData);
@@ -23,7 +24,7 @@ export const increaseOrders = async () => {
 
   const baseCoinType = currentDCAData.fields.base_coin_type;
   const quoteCoinType = currentDCAData.fields.quote_coin_type;
-  const addOrdersCount = 1;
+  const addOrdersCount = 3;
 
   const { tx, txRes } = await DCAManagerSingleton.getDCAIncreaseOrdersRemainingTransaction({
     publicKey: user,
@@ -36,10 +37,12 @@ export const increaseOrders = async () => {
     transaction,
   });
 
-  const res = await provider.devInspectTransactionBlock({
-    sender: sender,
-    transactionBlock: tx,
-  });
+  // const res = await provider.devInspectTransactionBlock({
+  //   sender: sender,
+  //   transactionBlock: tx,
+  // });
+
+  const res = await signAndExecuteTransaction(tx, delegateeKeypair);
 
   console.debug("txRes: ", txRes);
   console.debug("tx: ", tx);
