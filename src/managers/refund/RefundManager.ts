ec74dcff41941c71fbe5ce69172d6d1ade65c224
avernikoz <avernikoz@gmail.com>
2024-03-21 19:08:26 +0300

    change name of method

--- src/managers/refund/RefundManager.ts
+++ src/managers/refund/RefundManager.ts
@@ -389,7 +389,7 @@ export class RefundManagerSingleton {
     return { tx, txRes };
   }
 
-  public static getClaimRefundBoosted({
+  public static getClaimRefundBoostedTransaction({
     boostedClaimCap,
     poolObjectId,
 
