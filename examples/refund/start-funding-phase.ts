a080cd1d7fc36d3cacc32fbb85d4abad9a057299
avernikoz <avernikoz@gmail.com>
2024-03-25 11:36:18 +0300

    upd start-funding-phase

--- examples/refund/start-funding-phase.ts
+++ examples/refund/start-funding-phase.ts
@@ -1,17 +1,18 @@
 import { SUI_CLOCK_OBJECT_ID } from "@mysten/sui.js/utils";
 import { RefundManagerSingleton } from "../../src";
 import { keypair, signAndExecuteTransaction } from "../common";
+import { delegateeKeypair } from "../dca/common";
 
 // yarn ts-node examples/refund/start-funding-phase.ts
 (async () => {
   const fundingPhaseTxAndRes = RefundManagerSingleton.startFundingPhase({
     publisherObjectId: RefundManagerSingleton.REFUND_POOL_PUBLISHER_OBJECT_ID,
     poolObjectId: RefundManagerSingleton.REFUND_POOL_OBJECT_ID,
-    timeoutMilliseconds: 1711037809000, // Needs to be timestamp in milliseconds
+    timeoutMilliseconds: 1711305382000, // Needs to be timestamp in milliseconds
     clock: SUI_CLOCK_OBJECT_ID,
   });
 
-  const res = await signAndExecuteTransaction(fundingPhaseTxAndRes.tx, keypair);
+  const res = await signAndExecuteTransaction(fundingPhaseTxAndRes.tx, delegateeKeypair);
 
   // const res = await provider.devInspectTransactionBlock({
   //   sender: "0x935fd79d69b98dc87cd43d4112e621fe92967c7f33fa232ddf4f6351bb1b9a19",
