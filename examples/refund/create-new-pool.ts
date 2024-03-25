dfbe64906ff0fb36a2093db05cd32f825c2dff05
avernikoz <avernikoz@gmail.com>
2024-03-25 16:07:21 +0300

    upd create new pool

--- examples/refund/create-new-pool.ts
+++ examples/refund/create-new-pool.ts
@@ -1,11 +1,11 @@
 import { TransactionBlock } from "@mysten/sui.js/transactions";
-import { keypair, signAndExecuteTransaction } from "../common";
-import { delegateeKeypair } from "../dca/common";
+import { keypair, provider, signAndExecuteTransaction } from "../common";
+import { delegateeKeypair, delegateeUser } from "../dca/common";
 
 const REFUND_PACKAGE_ADDRESS = "0xf5cb872b2fea1881128e5a9435cefb176b728cac8477b27d259ed3f3ccb50197";
 const REFUND_POOL_PUBLISHER_OBJECT_ID = "0x16c2fc18be4e8fbe66272c9516e773d414cc5b5bb95c2a18fae2120dbfc7b760";
 
-// npm ts-node examples/addAddresses.ts
+// yarn ts-node examples/refund/create-new-pool.ts
 const processTransactions = async () => {
   const txb = new TransactionBlock();
 
@@ -14,11 +14,10 @@ const processTransactions = async () => {
     arguments: [txb.object(REFUND_POOL_PUBLISHER_OBJECT_ID)],
   });
 
-  //   const res = await provider.devInspectTransactionBlock({
-  //     transactionBlock: txb,
-  //     sender:
-  //       "0x935fd79d69b98dc87cd43d4112e621fe92967c7f33fa232ddf4f6351bb1b9a19",
-  //   });
+  // const res = await provider.devInspectTransactionBlock({
+  //   transactionBlock: txb,
+  //   sender: delegateeUser,
+  // });
 
   const res = await signAndExecuteTransaction(txb, delegateeKeypair);
 
