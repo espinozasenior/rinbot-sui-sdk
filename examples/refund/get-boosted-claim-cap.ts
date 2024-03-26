770244ba476762020d7e1cf55f38eb55366095ea
avernikoz <avernikoz@gmail.com>
2024-03-26 21:53:37 +0300

    fix examples

--- examples/refund/get-boosted-claim-cap.ts
+++ examples/refund/get-boosted-claim-cap.ts
@@ -7,7 +7,7 @@ import { suiProviderUrl, user } from "../common";
 
   const boostedClaimCap = await refundManager.getBoostedClaimCap({
     ownerAddress: "0xadae014d41a1aada7003921d741061f9ff42700a470a043f998ddf0c2dd64994",
-    newAddress: "0xadae014d41a1aada7003921d741061f9ff42700a470a043f998ddf0c2dd64994",
+    newAddress: "0x6104acdbd394d08ba97376747f286cd90e5ff2a9d445f1bf64405802bd711d89",
   });
   console.debug("boostedClaimCap: ", boostedClaimCap);
 })();
