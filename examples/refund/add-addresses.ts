8300e2f89291b82802aff8cdfa52e58d4f89cd4a
avernikoz <avernikoz@gmail.com>
2024-04-02 17:37:11 +0300

    update add-addresses example for refund

--- examples/refund/add-addresses.ts
+++ examples/refund/add-addresses.ts
@@ -4,33 +4,49 @@ import { keypair, provider, signAndExecuteTransaction } from "../common";
 import { delegateeKeypair, delegateeUser } from "../dca/common";
 import BigNumber from "bignumber.js";
 import { readDataFromJsonFile } from "../utils";
+import { splitBy } from "../../src/providers/utils/splitBy";
 
 // yarn ts-node examples/refund/add-addresses.ts
-export const addAddresses = async ({ addresses, amounts }: { addresses: string[]; amounts: string[] }) => {
+export const addAddresses = async ({
+  affectedAddressesAndAmounts,
+}: {
+  affectedAddressesAndAmounts: { affectedAddress: string; amount: string }[];
+}) => {
   const poolID = RefundManagerSingleton.REFUND_POOL_OBJECT_ID;
   const publisherID = RefundManagerSingleton.REFUND_POOL_PUBLISHER_OBJECT_ID;
 
-  const { tx, txRes } = RefundManagerSingleton.getAddAddressesTransaction({
-    publisherObjectId: publisherID,
-    poolObjectId: poolID,
-    addressesList: addresses,
-    amountsList: amounts,
-    transaction: new TransactionBlock(),
-  });
+  const CHUNK_SIZE = 500;
+  const affectedAddressesAndAmuntsChunks = splitBy(affectedAddressesAndAmounts, CHUNK_SIZE);
 
-  // const res = await provider.devInspectTransactionBlock({
-  //   transactionBlock: tx,
-  //   sender: delegateeUser,
-  // });
+  for (const affectedAddressesAndAmuntsChunk of affectedAddressesAndAmuntsChunks) {
+    const addresses: string[] = affectedAddressesAndAmuntsChunk.map((el) => el.affectedAddress);
+    const amounts: string[] = affectedAddressesAndAmuntsChunk.map((el) => el.amount);
 
-  const res = await signAndExecuteTransaction(tx, delegateeKeypair);
+    const { tx, txRes } = RefundManagerSingleton.getAddAddressesTransaction({
+      publisherObjectId: publisherID,
+      poolObjectId: poolID,
+      addressesList: addresses,
+      amountsList: amounts,
+      transaction: new TransactionBlock(),
+    });
 
-  // Assuming you want to do something with the result or just log it for now
-  console.log(res);
+    const res = await provider.devInspectTransactionBlock({
+      transactionBlock: tx,
+      sender: delegateeUser,
+    });
+
+    // const res = await signAndExecuteTransaction(tx, delegateeKeypair);
+
+    // Assuming you want to do something with the result or just log it for now
+    console.log("tx sending result: ", res);
+  }
+
+  // console.debug("addresses[0], amounts[0]", addresses[0], amounts[0]);
+  // console.debug("addresses[124], amounts[124]", addresses[124], amounts[124]);
 };
 
 (async () => {
-  const FILE_NAME = "refund-contract-affected-addresses-and-amounts-list";
+  const FILE_NAME = "refund-contract-affected-addresses-and-amounts-list-prod";
   const affectedAddressesAndAmounts = await readDataFromJsonFile(FILE_NAME);
 
   if (!affectedAddressesAndAmounts) {
@@ -41,17 +57,57 @@ export const addAddresses = async ({ addresses, amounts }: { addresses: string[]
     throw new Error("Wrong format of file (not an array)");
   }
 
+  if (!isItemListFormat(affectedAddressesAndAmounts)) {
+    throw new Error("Wrong shape of data for affected addresses list");
+  }
+
   const addresses: string[] = affectedAddressesAndAmounts.map((el) => el.affectedAddress);
   const amounts: string[] = affectedAddressesAndAmounts.map((el) => el.amount);
 
-  const totalAmountInMist = amounts
+  if (addresses.length !== amounts.length) {
+    throw new Error("Addresses and amounts list length is not equal to each other");
+  }
+
+  const baseAmountInMist = amounts
     .reduce((acc: BigNumber, el) => acc.plus(new BigNumber(el)), new BigNumber(0))
     .toString();
+  const baseAmountInSui = new BigNumber(baseAmountInMist).div(SUI_DENOMINATOR).toString();
 
+  const boostedAmountInMist = new BigNumber(baseAmountInMist).div(2).toString();
+  const boostedAmountInSui = new BigNumber(boostedAmountInMist).div(SUI_DENOMINATOR).toString();
+
+  const totalAmountInMist = new BigNumber(baseAmountInMist).plus(boostedAmountInMist).toString();
   const totalAmountInSui = new BigNumber(totalAmountInMist).div(SUI_DENOMINATOR).toString();
 
-  console.log("AMOUNT IN TOTAL (MIST): ", totalAmountInMist);
-  console.log("AMOUNT IN TOTAL (SUI): ", totalAmountInSui);
+  console.log("BASE AMOUNT FOR ALL WALLETS (MIST): ", baseAmountInMist);
+  console.log("BASE AMOUNT FOR ALL WALLETS (SUI): ", baseAmountInSui);
+  console.log("BOOSTED AMOUNT FOR ALL WALLETS (MIST): ", boostedAmountInMist);
+  console.log("BOOSTED AMOUNT FOR ALL WALLETS (SUI): ", boostedAmountInSui);
+  console.log("TOTAL AMOUNT FOR ALL WALLETS (MIST): ", totalAmountInMist);
+  console.log("TOTAL AMOUNT FOR ALL WALLETS (SUI): ", totalAmountInSui);
 
-  addAddresses({ addresses: addresses, amounts: amounts });
+  addAddresses({ affectedAddressesAndAmounts });
 })();
+
+interface Item {
+  affectedAddress: string;
+  amount: string;
+}
+
+// eslint-disable-next-line require-jsdoc
+export function isItemListFormat(obj: unknown): obj is Item[] {
+  if (!Array.isArray(obj)) return false;
+
+  for (const item of obj) {
+    if (
+      typeof item !== "object" ||
+      item === null ||
+      typeof item.affectedAddress !== "string" ||
+      typeof item.amount !== "string"
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
